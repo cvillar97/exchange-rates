637cb23eb031a3170ea5124e390d18a05f393c83
Cristian <cristianvillar252@gmail.com>
2023-10-05 13:49:58 -0300

    Adds createCurrenciesTable( )
    This will populate each <select> with the currencies symbols.

--- src/index.js
+++ src/index.js
@@ -16,6 +16,29 @@ function getCurrenciesList(date, baseCurrency) {
     .then((JSONresponse) => JSONresponse.rates);
 }
 
+function createCurrenciesTable() {
+  const tableContainer = document.querySelector(".table-container");
+  const tableBody = document.querySelector("#table-body");
+  const date = document.querySelector("#date").value;
+  const baseCurrency = document.querySelector("#base-currency").value;
+
+  getCurrenciesList(date, baseCurrency).then((currenciesList) => {
+    Object.keys(currenciesList).forEach((currency) => {
+      const currencyTr = document.createElement("tr");
+      const currencyTd = document.createElement("td");
+      currencyTd.textContent = currency;
+      const valueTd = document.createElement("td");
+      valueTd.textContent = currenciesList[currency];
+
+      currencyTr.appendChild(currencyTd);
+      currencyTr.appendChild(valueTd);
+
+      tableBody.appendChild(currencyTr);
+    });
+  });
+
+}
+
 function getAPIData() {
   return fetch(
     "https://api.apilayer.com/exchangerates_data/latest",
